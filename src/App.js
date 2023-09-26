eb2b80532b239052d4c329734e538555d3d0ed6a
Ede Mounika <edemounika99@gmail.com>
2023-09-26 16:35:08 +0530

    reddit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import RemoteJsonData from './RemoteJsonData';
-import Header from './Header';
-import Footer from './Footer';
-
+import RemoteJsonData from "./RemoteJsonData";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
-      < Header />
-      <RemoteJsonData />
-      <Footer/>
+        <Header />
+        <RemoteJsonData />
+        <Footer />
       </React.Fragment>
-      
-      
     </div>
   );
 }
 
 export default App;
-
-
-
